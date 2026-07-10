36da3e9e19c3cae5e219f2c0d711ada6be2ecee3
quqye666666 <lipsonqhs@outlook.com>
2026-07-10 22:38:28 +0800

    Add request logging for debugging Railway 502

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ const PORT = process.env.PORT || 3000;
 
 app.set('trust proxy', 1);
 
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} ${req.method} ${req.url}`);
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
