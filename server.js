83f769e55d56ff9cb4eb97f15410e6ce310d11eb
quqye666666 <lipsonqhs@outlook.com>
2026-07-11 18:13:06 +0800

    Fix SQLite datetime string literal: use single quotes

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(session({
 
 app.use((req, res, next) => {
   if (req.session.userId) {
-    db.prepare('UPDATE users SET last_active = datetime("now") WHERE id = ?').run(req.session.userId);
+    db.prepare('UPDATE users SET last_active = datetime('now') WHERE id = ?').run(req.session.userId);
   }
   next();
 });
